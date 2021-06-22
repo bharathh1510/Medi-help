6aba1268b561f75dfd633660fd18b7fd144f378f
bharath <jayabharath653@gmail.com>
2021-06-22 14:16:45 +0530

    22/6/21 fragments, signup added

--- Med_Life/app/src/main/java/com/example/med_life/Create.java
+++ Med_Life/app/src/main/java/com/example/med_life/Create.java
@@ -2,13 +2,36 @@ package com.example.med_life;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class Create extends AppCompatActivity {
-
+    TextView dob;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        dob=findViewById(R.id.dob);
+    }
+    public void dob(View view) {
+        Calendar c=Calendar.getInstance();
+        int year,month,day;
+        year=c.get(Calendar.YEAR);
+        month=c.get(Calendar.MONTH);
+        day=c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog dpd=new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        dob.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                    }
+                },year,month,day);
+        dpd.show();
+
     }
 }
--- Med_Life/app/src/main/java/com/example/med_life/MainActivity.java
+++ Med_Life/app/src/main/java/com/example/med_life/MainActivity.java
@@ -2,36 +2,32 @@ package com.example.med_life;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Adapter;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-import com.google.android.material.tabs.TabLayout;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void login(View view) {
-        startActivity(new Intent(MainActivity.this,Home.class));
     }
 
     public void signup(View view) {
         startActivity(new Intent(MainActivity.this,Create.class));
     }
 
-    public void dob(View view) {
-    }
+
 
     public void save(View view) {
     }
