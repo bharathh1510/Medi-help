75d3158b178174d8957c29abba5c0daecf7363db
bharath <jayabharath653@gmail.com>
2021-06-23 10:53:04 +0530

    updated

--- Med_Life/app/src/main/java/com/example/med_life/Home.java
+++ Med_Life/app/src/main/java/com/example/med_life/Home.java
@@ -1,5 +1,6 @@
 package com.example.med_life;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import androidx.fragment.app.Fragment;
@@ -7,21 +8,32 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Adapter;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Home extends AppCompatActivity {
     TabLayout tl;
     ViewPager vp;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         tl= findViewById(R.id.tl);
         vp=findViewById(R.id.vp);
+        auth=FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null){
+            startActivity(new Intent(this,Home.class));
+            finish();
+        }
         vp.setAdapter(new MyAdapter(getSupportFragmentManager(), 0) {
 
             @Override
@@ -46,7 +58,39 @@ public class Home extends AppCompatActivity {
         });
         tl.setupWithViewPager(vp);
     }
+
+    public void home(View view) {
+        startActivity(new Intent(this, Home.class));
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.profile:
+                Toast.makeText(this,"Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.contact:
+                Toast.makeText(this, "Contact", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.logout:
+                auth.signOut();
+                startActivity(new Intent(this,MainActivity.class));
+                finish();
+                break;
+
+
+        }
+        return true;
+    }
+}
 abstract class MyAdapter extends FragmentPagerAdapter{
     String[] title={"Book Now","Upcoming"};
 
--- Med_Life/app/src/main/java/com/example/med_life/MainActivity.java
+++ Med_Life/app/src/main/java/com/example/med_life/MainActivity.java
@@ -8,19 +8,53 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mail, pass;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mail=findViewById(R.id.email);
+        pass=findViewById(R.id.pass);
+        auth=FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null){
+            startActivity(new Intent(this,Home.class));
+            finish();
+        }
     }
 
     public void login(View view) {
+        String email=mail.getText().toString().trim();
+        String password= pass.getText().toString();
+        if (email.isEmpty()|password.isEmpty()){
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            auth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        startActivity(new Intent(MainActivity.this,Home.class));
+                        finish();
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
 
     public void signup(View view) {
